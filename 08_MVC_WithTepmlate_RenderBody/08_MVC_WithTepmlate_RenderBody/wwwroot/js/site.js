6798b0163897a771fb6174b53d1357cb8fdf67ed
veliseyrek <veli47seyrek47@gmail.com>
2022-04-06 09:56:21 +0300

    06.06.2022

---
+++ 08_MVC_WithTepmlate_RenderBody/08_MVC_WithTepmlate_RenderBody/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    $("li a").each(function () {
+        if ($(this).attr("href") == window.location.pathname) {
+            $(this).addClass("activeLink");
+            console.log($(this).addClass("activeLink"));
+        }
+    })
+    //$("li a").attr("href")
+    //console.log($("li a").attr("href"));
+    //console.log(window.location.pathname);
+})
